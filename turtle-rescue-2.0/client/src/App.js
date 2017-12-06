849e044c2798ee61ab2eaed650ab5f603015e557
DZamora54 <dantezamora54@gmail.com>
2017-12-05 19:29:54 -0500

    adds modal component

--- turtle-rescue-2.0/client/src/App.js
+++ turtle-rescue-2.0/client/src/App.js
@@ -1,14 +1,15 @@
-import React, { Component }from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
 // import PropTypes from "prop-types";
 
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Map from "./components/Map/Map";
 import Navbar from "./components/Navbar/Navbar";
+import FormModal from "./components/FormModal/FormModal";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   //checking login for local storage
@@ -23,18 +24,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Router>
-        <Auth>
-        <div className="wrapper">
-        <Navbar />
-          <Route exact path="/" component={Login} />
-          <Route exact path="/register" component={Register} />
-           <Route exact path="/map" component={Map} />
-           
-           {/* <MatchWhenAuthorized path="/report" component={ReportForm} />  */}
-        </div>
-        </Auth>
-      </Router>
+        <Router>
+          <Auth>
+            <div className="wrapper">
+              <Navbar />
+              <Route exact path="/" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/map" component={Map} />
+
+              {/* <MatchWhenAuthorized path="/report" component={ReportForm} />  */}
+              <FormModal>{<p> Hello </p>}</FormModal>
+            </div>
+          </Auth>
+        </Router>
       </div>
     );
   }
