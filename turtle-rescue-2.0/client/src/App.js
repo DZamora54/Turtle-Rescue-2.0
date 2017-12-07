6495536a9e3c7e73570cabf50fa835a3dae604c6
DZamora54 <dantezamora54@gmail.com>
2017-12-07 18:52:32 -0500

    updated TurtleCard

--- turtle-rescue-2.0/client/src/App.js
+++ turtle-rescue-2.0/client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
 import Auth from "./components/Auth/Auth";
 // import PropTypes from "prop-types";
 
@@ -8,10 +9,11 @@ import Register from "./pages/Register";
 import Map from "./components/Map/Map";
 import Navbar from "./components/Navbar/Navbar";
 import Directions from "./components/Directions/Directions";
-import './App.css';
-import FormModal from "./components/FormModal/FormModal";
 import "./App.css";
-
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import FormModal from "./components/FormModal/FormModal";
+import TurtleCard from "./components/TurtleCard/TurtleCard";
+import "./App.css";
 
 class App extends Component {
   //checking login for local storage
@@ -26,16 +28,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Router>
-        <Auth>
-        <div className="wrapper">
-        <Navbar />
+        <Router>
+          <Auth>
+            <div className="wrapper">
+              <Navbar />
 
-          <Route exact path="/" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/map" component={Map} />
-          <Route exact path="/directions" component={Directions} />
-           {/* <MatchWhenAuthorized path="/report" component={ReportForm} />  */}
+              <Route exact path="/" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/map" component={Map} />
+              <Route exact path="/directions" component={Directions} />
+              {/* <MatchWhenAuthorized path="/report" component={ReportForm} />  */}
+              <TurtleCard />
               <FormModal>{<p> Hello </p>}</FormModal>
             </div>
           </Auth>
--- turtle-rescue-2.0/client/src/components/TurtleCard/TurtleCard.js
+++ turtle-rescue-2.0/client/src/components/TurtleCard/TurtleCard.js
@@ -1,36 +1,12 @@
 import React from "react";
-import "./TurtleCard";
+import "./TurtleCard.css";
 
-const TurtleCard = () =>
-
-<Col m={6} s={12}>
-<Card className='blue-grey darken-1' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
-I am a very simple card.
-</Card>
-</Col>
-
-
-<!--TURTLE CARD-->
-			<div id="fullCard" class="col s12 m6">
-				<div id="turtle" class="card hoverable hide">
-					<div class="card-image">
-						<div id="map" class="col s12"></div>
-					</div>
-					<div class="card-content">
-						<span id="number" class="card-title activator green-text"><i class="material-icons right">more_vert</i></span>
-						<p id="reported"></p>
-					</div>
-					<div class="card sticky-action">
-						<div class="card-action right-align">
-							<button id="next-stage-btn" class="card-button green btn-flat yellow-text waves-effect waves-light" type="button" name="button">Done</button>
-						</div>
-					</div>
-					<div class="card-reveal">
-						<span class="card-title green-text">Comment<i class="material-icons right">close</i></span>
-						<p id="comment"></p>
-					</div>
-				</div>
-			</div>
-
+const TurtleCard = () => (
+  <div className="tcard">
+    <p>This is the turtle card</p>
+    <p>the map goes here</p>
+    <p>and the details go here</p>
+  </div>
+);
 
 export default TurtleCard;
