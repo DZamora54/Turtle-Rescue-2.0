a3a9f056256625e6321f109a730eceed2385937a
DZamora54 <dantezamora54@gmail.com>
2017-12-07 19:41:34 -0500

    updates for db

--- turtle-rescue-2.0/models/turtle.js
+++ turtle-rescue-2.0/models/turtle.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const turtleSchema = new Schema({
+  _id: {type: Schema.Types.ObjectId, required: true},
   dateTimeReported: { type: Date, default: Date.now },
   city: { type: String, required: true },
   state: { type: String, required: true, maxlength: 2, minlength: 2 },
   coordinates: { type: String, required: true },
   comments: { type: String },
-  name: { type: String, required: true},
-  phoneNumber: { type: String, required: true}
+  name: { type: String, required: true },
+  phoneNumber: { type: String, required: true }
 });
-
 
 const Turtle = mongoose.model("Turtle", turtleSchema);
 
