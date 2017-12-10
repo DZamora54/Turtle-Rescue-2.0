2ccc7c9a83c8c1d75c2b23b9e49e40bf7652bf7b
Colton Cmar <colton.cmar@gmail.com>
2017-12-10 09:04:33 -0500

    changes to to turtle.js

--- turtle-rescue-2.0/models/turtle.js
+++ turtle-rescue-2.0/models/turtle.js
@@ -6,7 +6,16 @@ const turtleSchema = new Schema({
   //city: { type: String, required: true },
   email: { type: String, required: true, match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]},
   coordinates: { type: String, required: true },
-  subject: { type: String, body: string },
+  subject: { type: String, validate: [
+    // Function takes in the new `longstring` value to be saved as an argument
+    function(input) {
+      // If this returns true, proceed. If not, return the error message below
+      return input.length >= 6;
+    },
+    // Error Message
+    "Subject description should be longer."
+  ]
+},
   name: { type: String, required: true},
   //phoneNumber: { type: String, required: true}
 });
